0b4449e2c155f92ff6c63d575336aba6051bac0a
sensenjaaa07 <jrianne87@gmail.com>
2026-09-19 19:54:11 +0800

    Test transaction date filtering for transfers

--- src/services/filterExpensesByDate.test.js
+++ src/services/filterExpensesByDate.test.js
@@ -21,3 +21,14 @@ test('filters by just a start date', () => {
   const filtered = filterExpensesByDate(expenses, '2025-02-01', null)
   assert.deepEqual(filtered.map((expense) => expense.id), ['2', '3'])
 })
+
+
+test('filters transfers by their transaction date', () => {
+  const transfers = [
+    { id: 't1', type: 'transfer', amount: 100, date: new Date('2025-01-20T00:00:00') },
+    { id: 't2', type: 'transfer', amount: 200, date: new Date('2025-02-10T00:00:00') },
+  ]
+
+  const filtered = filterExpensesByDate(transfers, '2025-02-01', '2025-02-28')
+  assert.deepEqual(filtered.map((transfer) => transfer.id), ['t2'])
+})
