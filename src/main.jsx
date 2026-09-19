555f8fb74d276fd2d6b966386235e986a894270b
sensenjaaa07 <jrianne87@gmail.com>
2026-09-19 19:48:52 +0800

    Normalize stylesheet import paths

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '../src/assets/styles/index.css'
+import './assets/styles/index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
