68f8fd14de6b29a3457ac2feca965b44ff3138f0
sensenjaaa07 <jrianne87@gmail.com>
2026-09-19 20:18:24 +0800

    Include Playwright config in Node lint rules

--- eslint.config.js
+++ eslint.config.js
@@ -19,7 +19,7 @@ export default defineConfig([
     },
   },
   {
-    files: ['api/**/*.js'],
+    files: ['api/**/*.js', 'playwright.config.js'],
     languageOptions: {
       globals: globals.node,
     },
